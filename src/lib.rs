85c1786bf6559dbf45ca834795fbcc59bf2b4686
Nikita L. Karavaev <nikita.lk@gmail.com>
2020-07-14 13:18:24 +0300

    trim3

--- src/lib.rs
+++ src/lib.rs
@@ -130,4 +130,56 @@ let l = _vec_data.len();
         return vec![top, left, right, bottom];
 }
 
+ #[wasm_bindgen]
+pub fn trim3(
+    ctx: &CanvasRenderingContext2d, width: u32, height: u32
+) -> Vec<u32> {
+    let data = ctx
+        .get_image_data(0.0, 0.0, width as f64, height as f64)
+        .unwrap();
+    let _vec_data = data.data().to_vec();
+
+let l = _vec_data.len();
+    
+    let mut top = 99999;
+    let mut left = 99999;
+    let mut right = 99999;
+    let mut bottom = 99999;
+    let mut x;
+    let mut y;
+
+
+    
+  for i in (0..l).step_by(4) {
+    if _vec_data[i + 3] != 0 {
+      x = (i as u32 / 4) % width;
+      y = !!(i as u32 / 4 / width);
+
+      if top == 99999 {
+        top = y;
+      }
+
+      if left == 99999 {
+        left = x;
+      } else if x < left {
+        left = x;
+      }
+
+      if right == 99999 {
+        right = x;
+      } else if right < x {
+        right = x;
+      }
+
+      if bottom == 99999 {
+        bottom = y;
+      } else if bottom < y {
+        bottom = y;
+      }
+    }
+  } 
+
+        return vec![top, left, right, bottom];
+}
+
    
