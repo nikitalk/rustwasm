b5801d852d91ef8821f07650a4657cee6c0c4381
Nikita L. Karavaev <nikita.lk@gmail.com>
2020-07-14 11:08:01 +0300

    ff

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
+use web_sys::{CanvasRenderingContext2d, OffscreenCanvas};
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -65,7 +66,68 @@ let l = pixels_data.len();
         bottom = y;
       }
     }
-  }
+  } 
 
         return vec![top, left, right, bottom];
     }
+
+
+    #[wasm_bindgen]
+pub fn trim2(
+    canvas: &OffscreenCanvas,
+    ctx: &CanvasRenderingContext2d,
+) -> Vec<u32> {
+    //set_panic_hook();
+    let width = canvas.width();
+    let height = canvas.height();
+
+    // let data: ImageData = ctx.get_image_data(0.0, 0.0, 100.0, 100.0).unwrap();
+    let data = ctx
+        .get_image_data(0.0, 0.0, width as f64, height as f64)
+        .unwrap();
+    let _vec_data = data.data().to_vec();
+
+let l = _vec_data.len();
+    
+    let mut top = 99999;
+    let mut left = 99999;
+    let mut right = 99999;
+    let mut bottom = 99999;
+    let mut x;
+    let mut y;
+
+
+    
+  for i in (0..l).step_by(4) {
+    if _vec_data[i + 3] != 0 {
+      x = (i as u32 / 4) % width;
+      y = !!(i as u32 / 4 / width);
+
+      if top == 99999 {
+        top = y;
+      }
+
+      if left == 99999 {
+        left = x;
+      } else if x < left {
+        left = x;
+      }
+
+      if right == 99999 {
+        right = x;
+      } else if right < x {
+        right = x;
+      }
+
+      if bottom == 99999 {
+        bottom = y;
+      } else if bottom < y {
+        bottom = y;
+      }
+    }
+  } 
+
+        return vec![top, left, right, bottom];
+}
+
+   
